4cb510f09f826a2e5c45f62165120b64b66253bc
Kaleb Cooper <thekalebcooper@gmail.com>
2021-09-03 23:15:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Swiftiator
+
+A description of this package.
---
+++ Tests/SwiftiatorTests/SwiftiatorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Swiftiator
+
+final class SwiftiatorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Swiftiator().text, "Hello, World!")
+    }
+}
